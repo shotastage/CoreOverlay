e3502ccd302b52daea2964b6d33be9c677f6ee42
Shota Shimazu <hornet.live.mf@gmail.com>
2022-06-09 10:06:12 +0900

    add new testcase

---
+++ Tests/SwiftlyDHTTests/CommonCryptoAlgorithmTests.swift
@@ -0,0 +1,28 @@
+//
+//  CommonCryptoAlgorithmTests.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/06/09.
+//
+
+import Foundation
+
+import XCTest
+@testable import SwiftlyDHT
+
+final class CommonCryptoAlgorithmTests: XCTestCase {
+
+    func testSHA1() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        let text = "HELLO_WORLD"
+
+        print("====== TEST CASE 1 ======")
+        print("TEXT: \(text)")
+        print("DIS:  \(CommonCryptoAlgorithm.sha1(data: text.data(using: .utf8)!))")
+
+        XCTAssertEqual(CommonCryptoAlgorithm.sha1(data: text.data(using: .utf8)!), "e94c4822769a728d4e106461b823328f5945f3e0")
+    }
+}
--- Tests/SwiftlyDHTTests/SocketConnectionTests.swift
+++ Tests/SwiftlyDHTTests/SocketConnectionTests.swift
@@ -1,19 +1,19 @@
 //
-//  File.swift
+//  SocketConnectionTests.swift
 //  
 //
-//  Created by Shota Shimazu on 2022/06/07.
+//  Created by Shota Shimazu on 2022/06/08.
 //
 
 import XCTest
 @testable import SwiftlyDHT
 
-final class UtilFuncsTests: XCTestCase {
+final class SocketConnectionTests: XCTestCase {
     func testXORDistance1() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        
+
         let first =  0b0010001010
         let second = 0b0010000010
 
@@ -26,16 +26,15 @@ final class UtilFuncsTests: XCTestCase {
         XCTAssertEqual(SwiftlyDHTMath.hummingDistance(first, second), 1)
     }
 
-
     func testXORDistance2() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        
+
         let first =  0b0010100010101011101
         let second = 0b0010111000101011101
 
-        
+
         print("====== TEST CASE 2 ======")
         print("IN:   \(first)")
         print("OUT:  \(second)")
@@ -43,21 +42,5 @@ final class UtilFuncsTests: XCTestCase {
         print("DIS:  \(SwiftlyDHTMath.hummingDistance(first, second))")
 
         XCTAssertEqual(SwiftlyDHTMath.hummingDistance(first, second), 3)
-    }
-
-    func testRandomID() throws {
-        print("====== TEST CASE 3 ======")
-
-        var previous = ""
-        var current = ""
-
-        for i in 1...100 {
-            previous = current
-
-            current = SwiftlyDHTMath.randomID()
-
-            print("\(i):  \(current)")
-            XCTAssertNotEqual(current, previous)
-        }
     }
 }
