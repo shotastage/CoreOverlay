eaa3d8542659d7b0869f36b9e6748162585dbc3a
Shota Shimazu <hornet.live.mf@gmail.com>
2022-06-10 20:55:31 +0900

    add some file

---
+++ Sources/CoreOverlay/InternetProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  InternetProtocol.swift
+//
+//
+//  Created by Shota Shimazu on 2022/06/08.
+//
+
+import Darwin
+
+public enum InternetNetworkProtocol {
+    case ipv4
+    case ipv6
+}
+
+public class COInternetNetwork {
+    
+    func getIp(protocol: InternetNetworkProtocol = .ipv4) -> String {
+        return ""
+    }
+}
---
+++ Sources/CoreOverlay/KHSperNode.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/06/14.
+//
+
+import Foundation
+
+
+public class KSuperNode {
+    
+    func initializeSuperNode() {
+        
+    }
+}
