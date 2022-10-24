eec44b628ef896521de81343a2a4740a6dcd82fd
shotastage <hornet.live.mf@gmail.com>
2022-10-25 01:44:57 +0900

    add Wasmer defs

---
+++ Sources/Wasmer/Wasmer.swift
@@ -0,0 +1,20 @@
+//
+//  Wasmer.swift
+//
+//
+//  Created by Shota Shimazu on 2022/10/25.
+//
+
+import Wasmer
+
+
+public enum Wasmer {}
+
+public extension Wasmer {
+  
+  static var version : ( major: Int, minor: Int, patch : Int ) {
+    return ( Int(wasmer_version_major()),
+             Int(wasmer_version_minor()),
+             Int(wasmer_version_patch()) )
+  }
+}
