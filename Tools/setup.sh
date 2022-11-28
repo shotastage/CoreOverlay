0169864ab2f971aaeaaaf8b69d7f7bd4b0f439c6
shotastage <hornet.live.mf@gmail.com>
2022-11-28 12:05:23 +0900

    add confirmation to script

--- Tools/setup.sh
+++ Tools/setup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+echo
+echo "Install Swift Protocol Buffers Libraries"
+echo 
+echo " - swift-protobuf"
+echo " - grpc-swift"
+echo
+echo "Press [RETURN] to continue:"
+read wait
+
 brew update
 brew install swift-protobuf grpc-swift
