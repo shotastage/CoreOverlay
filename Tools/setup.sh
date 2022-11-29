375d2510f8c2059e968ce96dc3e4a2f44d7c6675
shotastage <hornet.live.mf@gmail.com>
2022-11-29 23:18:41 +0900

    add some sample source

--- Tools/setup.sh
+++ Tools/setup.sh
@@ -5,9 +5,25 @@ echo "Install Swift Protocol Buffers Libraries"
 echo 
 echo " - swift-protobuf"
 echo " - grpc-swift"
+echo " - wabt"
+
 echo
 echo "Press [RETURN] to continue:"
 read wait
 
+
+check_and_install ()
+{
+    if type $0 > /dev/null 2>&1; then
+        echo $0 command is already installed.
+    else
+        brew install $1
+    fi
+}
+
 brew update
-brew install swift-protobuf grpc-swift
+
+# check_and_install protoc swift-protobuf
+# check_and_install protoc-gen-swift grpc-swift
+# check_and_install wat2wasm wabt
+brew install swift-protobuf grpc-swift wabt
