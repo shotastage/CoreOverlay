0a050689c2fa44dad48d34f634a46f376aa63fdc
shotastage <hornet.live.mf@gmail.com>
2022-12-03 22:27:55 +0900

    add sample actor source

--- Resources/sample/sample.swift
+++ Resources/sample/sample.swift
@@ -1,3 +1,16 @@
 func add(a: Int, b: Int) -> Int {
     return a + b
 }
+
+
+actor SampleActor {
+    let property = 1
+    
+    func method() -> Int {
+        return property + 1
+    }
+
+    func method2(msg: String) -> String {
+        return "Hello \(msg)"
+    }
+}
