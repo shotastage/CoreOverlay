2151efe03aa6656e49f98c3df5736e1855386167
shotastage <hornet.live.mf@gmail.com>
2022-12-10 15:14:03 +0900

    implement wasm runner framework

--- Sources/Runtime/WebAssembly.swift
+++ Sources/Runtime/WebAssembly.swift
@@ -9,21 +9,59 @@ import Foundation
 import WasmInterpreter
 
 
+/// WASM runtime backend selection
 public enum WARuntimeBackend {
     case wasmer
     case wasm3
     case jscore
-    case llvm
 }
 
+
+/// WASM Canister arguments wrapped object.
+public struct WARunnerArguments {
+    let variables: Dictionary<String, String>
+}
+
+
+/// WASM canister returns tuple object.
+public struct WARunnerReturns {
+    let store: Dictionary<Int, String>
+}
+
+
+/// WASMRunner protocol
+public protocol WASMRunner {
+    var programLoad: [UInt8] { get set }
+    func run(args: WARunnerArguments) -> WARunnerReturns
+}
+
+
+/// WASM runtime exposed object.
 open class WASMCanister {
     let backend: WARuntimeBackend = .wasm3
+    
+    
+
+    func initializeRunner(url: URL) -> some WASMRunner {
+        switch backend {
+        case .wasm3:
+            return try! WASM3Runner(file: url)
+        case .wasmer:
+            return try! WASM3Runner(file: url)
+        case .jscore:
+            return try! WASM3Runner(file: url)
+        }
+    }
+}
+
+/// WASM runners
+open class WASM3Runner: WASMRunner {
 
     // WASM VM interpreter instance
     private let _vm: WasmInterpreter
 
-    // bit loaded
-    private let programLoad: [UInt8]
+    public var programLoad: [UInt8]
+
 
     // Initializers
     public init(file: URL) throws {
@@ -35,9 +73,23 @@ open class WASMCanister {
     public func execute(_ first: Int, _ second: Int) throws -> Int {
         Int(try _vm.call("main", Int32(first), Int32(second)) as Int32)
     }
+
+
+    public func run(args: WARunnerArguments) -> WARunnerReturns {
+        // Now under construction...
+        return WARunnerReturns(store: [0: "SampleStore"])
+    }
 }
 
 
-class JSCoreRuntime: WASMCanister {
+class JSCoreRuntime: WASMRunner {
+    var programLoad: [UInt8] = []
     
+    init() {
+        print("Now under construction...")
+    }
+
+    func run(args: WARunnerArguments) -> WARunnerReturns {
+        return WARunnerReturns(store: [0: "Sample"])
+    }
 }
