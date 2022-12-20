1b46239e7e3b02c25cf8ce9dee9d50cae54e4c5c
shotastage <hornet.live.mf@gmail.com>
2022-12-20 10:40:26 +0900

    add inspector base

---
+++ Sources/CoreOverlay/WasmNortalize.swift
@@ -0,0 +1,31 @@
+//
+//  WasmNortlize.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/12/20.
+//
+
+import Foundation
+import OverlayFundation
+
+
+
+struct NotarlizeTable {
+    let objectIdentifier: String
+}
+
+
+class WasmNotarized {
+    
+    func nortalize() {
+        COLogger.info("Initializing wasm package notarization...")
+    }
+
+    func accept() {
+        COLogger.info("Accept notarizing...")
+    }
+
+    func deny() {
+        COLogger.info("Deny notarizing...")
+    }
+}
