a416ebd59374f0c22fdbcc270f82e0832a9aac2d
shotastage <hornet.live.mf@gmail.com>
2022-12-28 10:36:07 +0900

    add framework definitions

--- lib/overlaystd/src/logger.rs
+++ lib/overlaystd/src/logger.rs
@@ -1,4 +1,5 @@
 
+#[repr(C)]
 struct Logger {}
 
 impl Logger {
@@ -17,4 +18,12 @@ impl Logger {
     pub fn error(msg: String) {
         eprintln!("{}", msg);
     }
+}
+
+
+#[no_mangle]
+pub extern "C" fn logger_log(msg: *const c_char) {
+    let msg = unsafe { CStr::from_ptr(msg) };
+    let msg = msg.to_str().unwrap();
+    Logger::log(msg.to_string());
 }
