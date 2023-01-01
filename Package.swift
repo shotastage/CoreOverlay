b2bfded4d2f4b86caf44ea49b2a7ab6085f657c8
shotastage <hornet.live.mf@gmail.com>
2023-01-02 01:18:13 +0900

    fix val change mode

--- Package.swift
+++ Package.swift
@@ -4,6 +4,29 @@
 import Foundation
 import PackageDescription
 
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+
+enum EnviromentVal: String {
+    static let `default`: EnviromentVal = .production
+
+    case development, production
+    
+    static func get() -> EnviromentVal {
+        if let envPointer = getenv("COREOVERLAY_BUILD_MODE") {
+            let env = String(cString: envPointer)
+            return EnviromentVal(rawValue: env) ?? .default
+        } else {
+            return .default
+        }
+    }
+}
+
+
 func getRelativePathToHome() -> String {
     let fm = FileManager.default
     var relativePath = ""
@@ -24,11 +47,97 @@ func getRelativePathToHome() -> String {
     return relativePath
 }
 
+var targetDeps: [Target] = [
+    .systemLibrary(
+        name: "CLevelDB",
+        providers: [
+            .brew(["leveldb"]),
+        ]
+    ),
+    .binaryTarget(
+        name: "CWasmer",
+        url: "https://github.com/shotastage/CWasmer/releases/download/v0.0.1/CWasmer.xcframework.zip",
+        checksum: "b91a858ee7ff1ee9ec1fd0b9da126cd1bfde4e09f9ae75a4ad0364fdcbdd27f3"
+    ),
+    .target(
+        name: "CommonCrypt"
+    ),
+    .target(
+        name: "OverlayFundation"
+    ),
+    .target(
+        name: "OverlayDB",
+        dependencies: [
+            .product(name: "SQLite", package: "SQLite.swift"),
+        ]
+    ),
+    .target(
+        name: "Runtime",
+        dependencies: [
+            "CWasmer",
+            .product(name: "WasmInterpreter", package: "wasm-interpreter-apple"),
+        ]
+    ),
+    .target(
+        name: "Protobuf.Generated",
+        dependencies: [
+            .product(name: "GRPC", package: "grpc-swift"),
+        ]
+    ),
+    .target(
+        name: "CoreOverlay",
+        dependencies: [
+            "CommonCrypt",
+            "OverlayFundation",
+            "CoreOverlayEngine",
+            "Protobuf.Generated",
+            "Runtime",
+            .product(name: "GRPC", package: "grpc-swift"),
+            .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "WasmInterpreter", package: "wasm-interpreter-apple"),
+            // - .product(name: "LibP2P", package: "swift-libp2p"),
+        ]
+    ),
+    .executableTarget(
+        name: "CLI",
+        dependencies: [
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+        ]
+    ),
+    .testTarget(
+        name: "CoreOverlayTests",
+        dependencies: ["CoreOverlay"]
+    )
+]
+
 let cxxSettings: [CXXSetting] = [
     .headerSearchPath("."),
     .headerSearchPath("include"),
     .headerSearchPath("\(getRelativePathToHome()).wasmer/include"),
 ]
+
+
+switch EnviromentVal.get() {
+case .production:
+    print("PRODUCTION BUILD TARGET SELECTED!")
+    targetDeps.append(
+        Target.binaryTarget(
+            name: "CoreOverlayEngine",
+             url: "https://github.com/shotastage/CoreOverlay/releases/download/v0.0.4/bundle.zip",
+             checksum: "bfe73fa008052eafab0857c74e83895484f26aa344b7f96f1717ace6f9290843"
+        )
+    )
+case .development:
+    print("DEVELOPMENT BUILD TARGET SELECTED!")
+    targetDeps.append(
+        Target.binaryTarget(
+            name: "CoreOverlayEngine",
+            path: "./artifacts/CoreOverlayEngine.xcframework"
+        )
+    )
+}
+
 
 let package = Package(
     name: "CoreOverlay",
@@ -72,79 +181,6 @@ let package = Package(
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"),
         // .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.2"),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .systemLibrary(
-            name: "CLevelDB",
-            providers: [
-                .brew(["leveldb"]),
-            ]
-        ),
-        .binaryTarget(
-            name: "CWasmer",
-            url: "https://github.com/shotastage/CWasmer/releases/download/v0.0.1/CWasmer.xcframework.zip",
-            checksum: "b91a858ee7ff1ee9ec1fd0b9da126cd1bfde4e09f9ae75a4ad0364fdcbdd27f3"
-        ),
-        // - .binaryTarget(
-        //     name: "CoreOverlayEngine",
-        //     url: "https://github.com/shotastage/CoreOverlay/releases/download/v0.0.4/bundle.zip",
-        //     checksum: "bfe73fa008052eafab0857c74e83895484f26aa344b7f96f1717ace6f9290843"
-        // - ),
-        .binaryTarget(
-            name: "CoreOverlayEngine",
-            path: "./artifacts/CoreOverlayEngine.xcframework"
-        ),
-        .target(
-            name: "CommonCrypt"
-        ),
-        .target(
-            name: "OverlayFundation"
-        ),
-        .target(
-            name: "OverlayDB",
-            dependencies: [
-                .product(name: "SQLite", package: "SQLite.swift"),
-            ]
-        ),
-        .target(
-            name: "Runtime",
-            dependencies: [
-                "CWasmer",
-                .product(name: "WasmInterpreter", package: "wasm-interpreter-apple"),
-            ]
-        ),
-        .target(
-            name: "Protobuf.Generated",
-            dependencies: [
-                .product(name: "GRPC", package: "grpc-swift"),
-            ]
-        ),
-        .target(
-            name: "CoreOverlay",
-            dependencies: [
-                "CommonCrypt",
-                "OverlayFundation",
-                "CoreOverlayEngine",
-                "Protobuf.Generated",
-                "Runtime",
-                .product(name: "GRPC", package: "grpc-swift"),
-                .product(name: "Crypto", package: "swift-crypto"),
-                .product(name: "WasmInterpreter", package: "wasm-interpreter-apple"),
-                // - .product(name: "LibP2P", package: "swift-libp2p"),
-            ]
-        ),
-        .executableTarget(
-            name: "CLI",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
-            ]
-        ),
-        .testTarget(
-            name: "CoreOverlayTests",
-            dependencies: ["CoreOverlay"]
-        ),
-    ],
+    targets: targetDeps,
     cxxLanguageStandard: .cxx20
 )
