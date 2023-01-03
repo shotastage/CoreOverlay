e19d990a170ba0dde4ec44f1f3a9198990fe98c3
shotastage <hornet.live.mf@gmail.com>
2023-01-03 14:46:39 +0900

    add new math alg into full-rusted std lib

--- lib/overlaystd/src/lib.rs
+++ lib/overlaystd/src/lib.rs
@@ -2,6 +2,15 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+fn humming_distance(left: usize, right: usize) -> usize {
+    add(left, right)
+}
+
+#[no_mangle]
+pub extern "C" fn c_humming_distance(left: usize, right: usize) -> usize {
+    humming_distance(left, right)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
