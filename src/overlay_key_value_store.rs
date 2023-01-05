bc1e7fa4c380f1eeda2960a7a5578540f5e1218c
shotastage <hornet.live.mf@gmail.com>
2023-01-05 09:55:10 +0900

    add new cargo deps

--- src/overlay_key_value_store.rs
+++ src/overlay_key_value_store.rs
@@ -1,4 +1,10 @@
-pub fn kad_kvs() {
-    println!("KAD_KVS");
-    println!("TODO: IMPL BASIC KAD KVS");
+use libp2p::{identity, PeerId};
+use std::error::Error;
+
+pub fn network_ident() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    Ok(())
 }
