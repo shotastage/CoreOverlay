74273869c9fcf7d085fd25ee517e000961769f4e
shotastage <hornet.live.mf@gmail.com>
2023-01-16 22:48:30 +0900

    integrate cbigen into build.rs

---
+++ build.rs
@@ -0,0 +1,9 @@
+extern crate cbindgen;
+
+fn main() {
+    cbindgen::Builder::new()
+      .with_crate(".")
+      .generate()
+      .expect("Unable to generate bindings")
+      .write_to_file("engine.h");
+}
