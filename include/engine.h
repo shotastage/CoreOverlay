2326b4fe5d8f57f6e1d7c3ac6bf462d9044efa09
shotastage <hornet.live.mf@gmail.com>
2023-01-17 01:20:33 +0900

    fix header generate path invalidation

--- include/engine.h
+++ include/engine.h
@@ -1,15 +1,18 @@
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+extern "C" {
 
 int64_t add(int64_t first, int64_t second);
 
-const char *ovr_arch(void);
+const char *ovr_arch();
 
-const char *ovr_os(void);
+const char *ovr_os();
 
-const char *ovr_user_agent(void);
+const char *ovr_user_agent();
 
 void ovry_log(const char *msg);
 
@@ -20,3 +23,5 @@ void ovry_error(const char *msg);
 void c_exec_wasm_text_module(const char *wasm_text, const char *main_fn);
 
 void c_exec_wasm_native_module(const unsigned char *data, uintptr_t data_length);
+
+} // extern "C"
