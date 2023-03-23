d38bad4aa9c76a785e74d6b0162d53d25e328a6e
shotastage <hornet.live.mf@gmail.com>
2023-03-23 14:55:42 +0900

    add some test code

--- src/arch.rs
+++ src/arch.rs
@@ -25,3 +25,40 @@ pub extern "C" fn ovr_user_agent() -> *const c_char {
 
     return user_agent.into_raw();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ffi::CStr;
+    use std::env;
+
+    #[test]
+    fn test_ovr_arch() {
+        let arch_ptr = ovr_arch();
+        let arch_cstr = unsafe { CStr::from_ptr(arch_ptr) };
+        let arch_str = arch_cstr.to_str().unwrap();
+
+        assert_eq!(env::consts::ARCH, arch_str);
+        unsafe { CString::from_raw(arch_ptr as *mut c_char) };
+    }
+
+    #[test]
+    fn test_ovr_os() {
+        let os_ptr = ovr_os();
+        let os_cstr = unsafe { CStr::from_ptr(os_ptr) };
+        let os_str = os_cstr.to_str().unwrap();
+
+        assert_eq!(env::consts::OS, os_str);
+        unsafe { CString::from_raw(os_ptr as *mut c_char) };
+    }
+
+    #[test]
+    fn test_ovr_user_agent() {
+        let user_agent_ptr = ovr_user_agent();
+        let user_agent_cstr = unsafe { CStr::from_ptr(user_agent_ptr) };
+        let user_agent_str = user_agent_cstr.to_str().unwrap();
+
+        assert_eq!("OVERLAY/0.1.0 COREOVERLAY/0.1.0 DHT/Kademlia WASMER/3.1.0", user_agent_str);
+        unsafe { CString::from_raw(user_agent_ptr as *mut c_char) };
+    }
+}
