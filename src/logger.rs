37dfab45665cefafc2bb3280ede4f5efae716dd8
shotastage <hornet.live.mf@gmail.com>
2023-03-27 15:58:58 +0900

    revise

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,11 @@
 use std::ffi::c_char;
 
 #[no_mangle]
+pub extern "C" fn ovry_info(msg: *const c_char) {
+    println!("{:?}", msg);
+}
+
+#[no_mangle]
 pub extern "C" fn ovry_log(msg: *const c_char) {
     println!("{:?}", msg);
 }
--- src/wasmer_integration.rs
+++ src/wasmer_integration.rs
@@ -4,6 +4,21 @@ use std::ffi::CStr;
 use std::os::raw::{c_char, c_uchar};
 use wasmer::{imports, EngineBuilder, Instance, Module, Store, Value};
 
+pub fn wasmer_exec(file_binary: &[u8]) -> anyhow::Result<()> {
+    let engine = EngineBuilder::headless();
+    let mut store = Store::new(engine);
+    let module = Module::from_binary(&store, file_binary)?;
+
+    let import_object = imports! {};
+
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let sum = instance.exports.get_function("sum")?;
+    let _results = sum.call(&mut store, &[Value::I32(1), Value::I32(2)])?;
+
+    Ok(())
+}
+
 // Rust function that calls the wasm text module
 fn wasm_text_exec(wat_module: &str, main_fn: &str) -> anyhow::Result<()> {
     let mut store = Store::default();
