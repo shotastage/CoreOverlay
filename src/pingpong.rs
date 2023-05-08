734b22f486df7135794fc3efa0c73339c8566787
shotastage <hornet.live.mf@gmail.com>
2023-05-09 00:19:45 +0900

    make ping func to be able to call inside the C code

--- src/pingpong.rs
+++ src/pingpong.rs
@@ -1,3 +1,5 @@
-pub fn overlay_ping(listen_on: String) {
-    println!("OVERLAY_PING: {}", listen_on)
+#[no_mangle]
+pub extern "C" fn overlay_ping(listen_on: *const libc::c_char) {
+    let listen_on = unsafe { std::ffi::CStr::from_ptr(listen_on).to_string_lossy().into_owned() };
+    println!("OVERLAY_PING: {}", listen_on);
 }
