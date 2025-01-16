f72603b052809f08bdd446c1b1d0655ec5181e06
shotastage <shota.shimazu@magicalsoft.app>
2025-01-16 23:17:03 +0900

    Refactor overlaystd module: add logger and platform functionality, remove cologger module

--- lib/overlaystd/src/lib.rs
+++ lib/overlaystd/src/lib.rs
@@ -1,23 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-fn humming_distance(left: usize, right: usize) -> usize {
-    add(left, right)
-}
-
-#[no_mangle]
-pub extern "C" fn c_humming_distance(left: usize, right: usize) -> usize {
-    humming_distance(left, right)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod logger;
+pub mod platform;
--- lib/overlaystd/src/platform.rs
+++ lib/overlaystd/src/platform.rs
@@ -1,12 +1,41 @@
-struct Platform {}
+use std::env;
 
-impl Platform {
-    pub fn new() -> Self {
-        Self {}
+pub fn ovr_arch() -> &'static str {
+    env::consts::ARCH
+}
+
+pub fn ovr_os() -> &'static str {
+    env::consts::OS
+}
+
+pub fn ovr_user_agent() -> &'static str {
+    "OVERLAY/0.1.0 COREOVERLAY/0.1.0 DHT/Kademlia WASMER/3.1.0"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_ovr_arch() {
+        let arch_str = ovr_arch();
+
+        println!("arch_str: {}", arch_str);
+        assert_eq!(env::consts::ARCH, arch_str);
     }
 
-    pub fn get_platform() -> String {
-        let platform = "macOS";
-        platform.to_string()
+    #[test]
+    fn test_ovr_os() {
+        let os_str = ovr_os();
+        println!("os_str: {}", os_str);
+        assert_eq!(env::consts::OS, os_str);
+    }
+
+    #[test]
+    fn test_ovr_user_agent() {
+        let user_agent_str = ovr_user_agent();
+        println!("user_agent_str: {}", user_agent_str);
+        assert_eq!("OVERLAY/0.1.0 COREOVERLAY/0.1.0 DHT/Kademlia WASMER/3.1.0", user_agent_str);
     }
 }
