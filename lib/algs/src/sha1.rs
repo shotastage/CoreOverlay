1fe22cf8cc174dc55f7850aa82c6a8fe2f6a2f4d
shotastage <shota.shimazu@magicalsoft.app>
2025-01-16 21:39:23 +0900

    Remove unused calculate_file_sha1 function from SHA1 module

--- lib/algs/src/sha1.rs
+++ lib/algs/src/sha1.rs
@@ -8,27 +8,6 @@ pub fn calculate_sha1(text: &str) -> String {
     format!("{:x}", result)
 }
 
-// Function to calculate SHA1 hash from a file
-pub fn calculate_file_sha1(path: &str) -> std::io::Result<String> {
-    use std::fs::File;
-    use std::io::Read;
-
-    let mut file = File::open(path)?;
-    let mut hasher = Sha1::new();
-    let mut buffer = [0; 1024];
-
-    loop {
-        let bytes_read = file.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-        hasher.update(&buffer[..bytes_read]);
-    }
-
-    let result = hasher.finalize();
-    Ok(format!("{:x}", result))
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
