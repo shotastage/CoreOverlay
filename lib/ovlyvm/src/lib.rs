ec5a6333fa975a8543dc40a79b182fed8b25c425
shotastage <shota.shimazu@magicalsoft.app>
2025-01-21 01:17:44 +0900

    Remove unused import of Function from wasmer in lib.rs for cleaner code

--- lib/ovlyvm/src/lib.rs
+++ lib/ovlyvm/src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
 use std::path::Path;
-use wasmer::{Engine, Function, Imports, Instance, Memory, Module, Store, TypedFunction, WasmPtr};
+use wasmer::{Engine, Imports, Instance, Memory, Module, Store, TypedFunction, WasmPtr};
 
 /// WasmRunner is a struct for managing the execution of .wasm files
 pub struct WasmRunner {
