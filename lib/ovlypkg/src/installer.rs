f6cc0e72bc52dd933300edf4e8484f8621edb468
shotastage <shota.shimazu@magicalsoft.app>
2025-01-21 01:46:13 +0900

    Enhance documentation for fake_install and install_package_to_dht functions

--- lib/ovlypkg/src/installer.rs
+++ lib/ovlypkg/src/installer.rs
@@ -1,11 +1,32 @@
 use devhelper::under_construction;
 
-pub fn fake_install() {
+/// Fake install is the function that is used to install the package
+///
+/// # Arguments
+/// - `target`: target ovpkg to install
+///
+/// # Returns
+/// - Always returns 0
+pub fn fake_install(target: &str) -> usize {
     under_construction::message();
     println!("Fake install");
+    println!("{}", target);
+
+    return 0;
 }
 
-pub fn install_package_to_dht() {
+
+/// install_package_to_dht is the function that performs a production install to a running DHT network on CoreOverlay.
+///
+/// # Arguments
+/// - `target`: target ovpkg to install
+///
+/// # Returns
+/// - Always returns 0
+pub fn install_package_to_dht(target: &str) -> usize {
     under_construction::message();
     println!("Install package to DHT");
+    println!("{}", target);
+
+    return 0;
 }
