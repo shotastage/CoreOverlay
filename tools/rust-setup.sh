08f9a1b7167ac41ed53a79596bed4a27e7fc22bb
shotastage <shota.shimazu@magicalsoft.app>
2025-01-30 15:48:58 +0900

    Update Rust toolchain configuration and remove obsolete setup scripts

--- tools/rust-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Current installed Rust build targets:"
-rustup target list | grep apple
-rustup target add aarch64-apple-ios aarch64-apple-ios-sim
--- tools/setup-coreoverlay-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to setup the coreoverlay for the CLI
-echo "Now under construction..."
-git clone https://github.com/shotastage/CoreOverlay.git
-cd CoreOverlay
-swift build
